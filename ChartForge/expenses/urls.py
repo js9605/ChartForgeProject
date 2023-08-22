aef9b58fba67588d5190c6678acae87bf8135771
Jacek <jacekszwarc1@gmail.com>
2023-08-22 18:13:33 +0200

    Minor fix
    Fixing typos in html files

--- ChartForge/expenses/urls.py
+++ ChartForge/expenses/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="expenses"),
-    path('add-expense', views.add_expense, name="add-expenses")
+    path('add_expense', views.add_expense, name="add_expenses")
 
 ]
